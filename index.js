d688b04eafe0dee96661416c0769eb82d699c6f7
Chris Lebbano <chris.lebbano@gmail.com>
2017-03-21 09:26:46 -0400

    fixed bug with image and buttons

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code below this line
+
+/*
+http://fillmurray.com/100/100
+
+http://fillmurray.com/400/400
+*/
+
+// Cache the button element.
+const smallButton = document.querySelector('.smaller');
+const bigButton = document.querySelector('.larger');
+
+function smallClick(event){
+  let image = document.querySelector('p img');
+  image.src = "http://fillmurray.com/100/100";
+}
+
+function bigClick(event){
+  let image = document.querySelector('p img');
+  image.src = "http://fillmurray.com/400/400";
+}
+
+smallButton.addEventListener('click', smallClick);
+bigButton.addEventListener('click', bigClick);

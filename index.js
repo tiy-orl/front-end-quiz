e8a53b5c849464b18fa4a6ff08afefad2346f521
Tyler Davis <tyjdavis@outlook.com>
2017-03-21 09:52:01 -0400

    fixed issue

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code below this line
+function changeSmallPicture (event) {
+
+    let myImage = document.querySelector('p img')
+      myImage.src = "http://fillmurray.com/100/100";
+    }
+
+    let buttonSmall = document.querySelector('button.smaller');
+        buttonSmall.addEventListener('click', changeSmallPicture);
+
+
+  function changeLargePicture (event) {
+
+    let myImage = document.querySelector('p img')
+      myImage.src = "http://fillmurray.com/400/400";
+    }
+
+    let buttonLarge = document.querySelector('button.larger');
+        buttonLarge.addEventListener('click', changeLargePicture);

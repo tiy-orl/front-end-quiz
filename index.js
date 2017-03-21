6123b33cf54f13a9c63d05e4af17cc0d4e9efdc5
Chris Lebbano <chris.lebbano@gmail.com>
2017-03-21 09:33:28 -0400

    fixed Url

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ http://fillmurray.com/100/100
 http://fillmurray.com/400/400
 */
 
+const bigUrl = 'http://fillmurray.com/400/400';
+const smallUrl = 'http://fillmurray.com/100/100';
+
 // Cache the button element.
 const smallButton = document.querySelector('.smaller');
 const bigButton = document.querySelector('.larger');
 
 function smallClick(event){
   let image = document.querySelector('p img');
-  image.src = "http://fillmurray.com/100/100";
+  image.src = smallUrl;
 }
 
 function bigClick(event){
   let image = document.querySelector('p img');
-  image.src = "http://fillmurray.com/400/400";
+  image.src = bigUrl;
 }
 
 smallButton.addEventListener('click', smallClick);

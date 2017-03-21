71f5af414b59939592c5d2132a089bf5f614e397
Marvin Filus <marvinfi@Marvins-MacBook-Air.local>
2017-03-21 09:52:32 -0400

    Murrays

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code below this line
+
+
+    function smallMurray () {
+      let myImage = document.querySelector('p img');
+      myImage.src = 'http://www.fillmurray.com/100/100';
+    }
+
+    let button = document.querySelector('.smaller');
+    button.addEventListener('click', smallMurray);
+
+function bigMurray () {
+  let myImage = document.querySelector('p img');
+   myImage.src = 'http://www.fillmurray.com/400/400';
+
+}
+    let button2 = document.querySelector('.larger');
+    button2.addEventListener('click', bigMurray);

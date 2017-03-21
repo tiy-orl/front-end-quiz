b40e8a0143c722cfefaf365069bd1a31f56c12d1
Your Name <you@example.com>
2017-03-21 09:53:48 -0400

    buttons work

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code below this line
+function showSmallPic (){
+  console.log("http://fillmurray.com/100/100");
+let picElement= document.querySelector("p img");
+  picElement.src="http://fillmurray.com/100/100";
+}
+function showBigPic (){
+  console.log( "http://fillmurray.com/400/400");
+let picElement= document.querySelector("p img");
+picElement.src="http://fillmurray.com/400/400";
+}
+
+let smallPic= document.querySelector(".smaller");
+smallPic.addEventListener("click", showSmallPic);
+
+let bigPic= document.querySelector(".larger");
+bigPic.addEventListener("click", showBigPic);

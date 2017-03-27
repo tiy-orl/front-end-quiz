5c642cbeac31259db42ccbe29d6061f47f551fb8
Tyler Davis <tyjdavis@outlook.com>
2017-03-27 09:11:40 -0400

    quiz

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 // code below this line
-function changeSmallPicture (event) {
 
-    let myImage = document.querySelector('p img')
-      myImage.src = "http://fillmurray.com/100/100";
-    }
+let myImage = document.querySelector('p img')
 
-    let buttonSmall = document.querySelector('button.smaller');
-        buttonSmall.addEventListener('click', changeSmallPicture);
+function changeSmallPicture () {
+  myImage.src = "http://fillmurray.com/100/100";
+}
+
+let buttonSmall = document.querySelector('button.smaller');
+  buttonSmall.addEventListener('click', changeSmallPicture);
 
 
-  function changeLargePicture (event) {
+function changeLargePicture () {
+  myImage.src = "http://fillmurray.com/400/400";
+}
 
-    let myImage = document.querySelector('p img')
-      myImage.src = "http://fillmurray.com/400/400";
-    }
-
-    let buttonLarge = document.querySelector('button.larger');
-        buttonLarge.addEventListener('click', changeLargePicture);
+let buttonLarge = document.querySelector('button.larger');
+  buttonLarge.addEventListener('click', changeLargePicture);

ac9e3b86cd0e81e1462d4db8689289c30c48923a
Tyler Davis <tyjdavis@outlook.com>
2017-03-27 09:22:15 -0400

    monday changes

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 // code below this line
-<<<<<<< HEAD
 
-let myImage = document.querySelector('p img')
-
-function changeSmallPicture () {
-  myImage.src = "http://fillmurray.com/100/100";
-}
-
-let buttonSmall = document.querySelector('button.smaller');
-  buttonSmall.addEventListener('click', changeSmallPicture);
-
-
-function changeLargePicture () {
-  myImage.src = "http://fillmurray.com/400/400";
-}
-
-let buttonLarge = document.querySelector('button.larger');
-  buttonLarge.addEventListener('click', changeLargePicture);
-=======
 /*function showSmallPic (){
   console.log("http://fillmurray.com/100/100");
 let picElement= document.querySelector("p img");
@@ -41,4 +23,3 @@ for (let i=0; i < sizeButtons.length; i++){
 }
 //let bigPic= document.querySelector(".larger");
 //bigPic.addEventListener("click", showBigPic);
->>>>>>> 24973dcf25f6bbf637f8fda6cef5c586a7f369c7

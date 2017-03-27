0c0919fc8e3942c4cfed4845e29fa2b0263d4d17
Dwane <Dwane@hiphopandcode.com>
2017-03-27 09:58:11 -0400

    came up with a nested loop solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,13 @@
 let printValue;
 for (i=1; i<=100; i++){
-  printValue = i;
-  if (i%3 == 0){
-    printValue = "fizz";
-  }
-  if (i%5 == 0){
-    printValue = "buzz";
-  }
   if (i%15 == 0){
     printValue = "fizzbuzz";
+  }else if (i%3 == 0){
+    printValue = "fizz";
+  }else if (i%5 == 0){
+    printValue = "buzz";
+  }else {
+    printValue = i
   }
   console.log(printValue);
 }

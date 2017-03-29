03065c98512c6e522b719309bcd2b35706a33ad0
Chris Lebbano <chris.lebbano@gmail.com>
2017-03-29 09:41:17 -0400

    fixed issue

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-
-// code below this line
-/*function showSmallPic (){
-  console.log("http://fillmurray.com/100/100");
-let picElement= document.querySelector("p img");
-  picElement.src="http://fillmurray.com/100/100";
-}*/
-function showPicSize (event){
-  let button = event.target;
-  let whereThePictureGoes = document.querySelector("p img");
-  let size = button.dataset.size;
-  whereThePictureGoes.src = "http://fillmurray.com/"+size+"/"+size;
-/*  console.log( "http://fillmurray.com/400/400");
-let picElement= document.querySelector("p img");
-picElement.src="http://fillmurray.com/400/400";*/
-}
-
-let sizeButtons= document.querySelectorAll("button");
-//smallPic.addEventListener("click", showSmallPic);
-for (let i=0; i < sizeButtons.length; i++){
-  let button = sizeButtons[i];
-  button.addEventListener("click", showPicSize);
-}
-//let bigPic= document.querySelector(".larger");
-//bigPic.addEventListener("click", showBigPic);
+//
+// // code below this line
+// /*function showSmallPic (){
+//   console.log("http://fillmurray.com/100/100");
+// let picElement= document.querySelector("p img");
+//   picElement.src="http://fillmurray.com/100/100";
+// }*/
+// function showPicSize (event){
+//   let button = event.target;
+//   let whereThePictureGoes = document.querySelector("p img");
+//   let size = button.dataset.size;
+//   whereThePictureGoes.src = "http://fillmurray.com/"+size+"/"+size;
+// /*  console.log( "http://fillmurray.com/400/400");
+// let picElement= document.querySelector("p img");
+// picElement.src="http://fillmurray.com/400/400";*/
+// }
+//
+/// //smallPic.addEventListener("click", showSmallPic);
+// for (let i=0; i < sizeButtons.length; i++){
+//   let button = sizeButtons[i];
+//   button.addEventListener("click", showPicSize);
+// }
+// //let bigPic= document.querySelector(".larger");
+// //bigPic.addEventListener("click", showBigPic);

4934639aed78ac9c8c02149937fe1e01126c333d
Dwane <Dwane@hiphopandcode.com>
2017-04-03 10:10:47 -0400

    Add a coordinate finder #55

---
+++ movementToCoordinates.js
@@ -0,0 +1,23 @@
+function movements(directions){
+  let directionSplit = directions.split(' ');
+  let northSouth = 0;
+  let eastWest = 0;
+  let heading = 1;
+  for (i=0; i<directionSplit.length; i++){
+    let coordinateNumber = directionSplit[i].slice(0,directionSplit[i].length-1);
+    let coordinateDirection = directionSplit[i].slice(directionSplit[i].length-1,directionSplit[i].length);
+    if ((coordinateDirection == "S") || (coordinateDirection == "W")){
+      heading = -1;
+    }else{
+      heading =1;
+    }
+    if (directionSplit[i].includes("N") || directionSplit[i].includes("S")){
+      //if
+      northSouth = northSouth + (coordinateNumber*heading)
+    } else {
+      eastWest = eastWest + (coordinateNumber*heading)
+    }
+  }
+  console.log ("["+eastWest+","+northSouth+"]");
+}
+movements("3N 12S");

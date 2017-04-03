ad0d7dde4b1ccbc11421eb78987bf9a0b946177f
Chris Lebbano <chris.lebbano@gmail.com>
2017-04-03 11:07:53 -0400

    removed more comments

--- movementToCoordinates.js
+++ movementToCoordinates.js
@@ -1,21 +1,4 @@
-/*
-
-Write a function that takes a string of movements in the four compass
-directions and returns a pair of coordinates indicating how far (east, north)
-the movements would take you. For example:
-
-yourFunction("1N") would return [0, 1], indicating 0 movement east and 1 movement north
-yourFunction("2S 3E") would return [3, -2], indicating 3 movement east and -2 movement north
-yourFunction("3N 5E 1S 2W 2S 3W") would return [0, 0]
-save your function in movementToCoordinates.js
-
-*/
-
 !function(){
-
-  const parseString = (str) => {
-    return str.split(' ');
-  }
 
   const parseArray = (arr) => {
     let coordinates = [0,0];
@@ -34,16 +17,14 @@ save your function in movementToCoordinates.js
   }
 
   const findCoordinates = (str) => {
-    let array = parseString(str);
+    let array = str.split(' ');
     return parseArray(array);
   }
 
-  let test = "33N 5E 01S 02W 2S 3W";
+  let test = "3N 5E 01S 02W 2S 3W";
 
   let testCoor = findCoordinates(test);
 
   console.log(testCoor);
-
-
 
 }();

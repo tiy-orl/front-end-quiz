c424d177bf3f350f7c42cc938a90613e8a7731a7
Chris Lebbano <chris.lebbano@gmail.com>
2017-04-06 09:33:27 -0400

    Issue for Thurs

---
+++ englishify.js
@@ -0,0 +1,43 @@
+// Write a function that can convert a number into words -
+//
+// englishify(1) returns "one"
+// englishify(12) returns "twelve"
+// Make it work for any integer from zero to twenty. Save in a file called englishify.js.
+
+
+let numbers = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty'
+}
+
+function englishify(num) {
+  // .isNaN()
+  // Number.prototype.toLocaleString()
+  // return english word.
+
+  if (num >= 0 && num <= 20) {
+    return numbers[num];
+  }
+  return 'Please enter a number between 0 and zero inclusive.';
+}
+
+console.log( englishify(21) );

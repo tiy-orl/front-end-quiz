c14159085b4719aea2c2c100633e7d44bdeb7afd
Marvin Filus <marvinfi@Marvins-MacBook-Air.local>
2017-04-06 09:41:59 -0400

    quiz

---
+++ englishify.js
@@ -0,0 +1,6 @@
+
+let words = [ 'zero' , 'one' , 'two' , 'three' , 'four' , 'five' , 'six' , 'seven' , 'eight' , 'nine' , 'ten', 'eleven', 'twelve' , ' thirteen', 'fourteen', 'fifteen' , 'sixteen' , 'seventeen', 'eighteen' , 'nineteen', 'twenty' ]
+function englishify (number){
+  console.log(words[number])
+}
+englishify(19)

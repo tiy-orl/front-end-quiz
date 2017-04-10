fde702a8565e5d0e6f9d159ead8a2bc5b93327b8
Dwane <Dwane@hiphopandcode.com>
2017-04-10 10:16:46 -0400

    changeMaker.js

--- changeMaker.js
+++ changeMaker.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-function changeMaker(amountPaid, purchasePrice){
-  let coins = {quarters: 0, dimes: 0, nickels: 0, pennies:0}
-  let change = Math.abs(purchasePrice - amountPaid);
-  while (change > 0.01){
-    if (change >= .25){
-      coins.quarters++;
-      change -= .25;
-    } else if (change >= .10){
-      coins.dimes++;
-      change -= .10;
-    } else if (change >= .05){
-      coins.nickels++;
-      change -= .05;
-    } else {
-      coins.pennies++;
-      change -= .01;
-    }
-  }
-  console.log(coins);
-}
-changeMaker(1.74,2);
-=======
 function changeMaker (amountPaid, purchasePrice) {
   let changeInPennies = (amountPaid - purchasePrice) * 100;
   const coinValues = {
@@ -36,4 +13,3 @@ function changeMaker (amountPaid, purchasePrice) {
     changeInPennies -= numberOfCoins * coinValues[coinName];
   });
   return change;
->>>>>>> bc36b6de7c1482a3e8e255c97dd179697e2a9b54

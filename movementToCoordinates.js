c3dc60f07660a18430828210c6b62de3433d33c3
Dwane <Dwane@hiphopandcode.com>
2017-04-10 10:24:41 -0400

    movementToCoordinates.js

--- movementToCoordinates.js
+++ movementToCoordinates.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-function movements(directions){
-  let directionSplit = directions.split(' ');
-  let northSouth = 0;
-  let eastWest = 0;
-  let heading = 1;
-  for (i=0; i<directionSplit.length; i++){
-    let coordinateNumber = directionSplit[i].slice(0,directionSplit[i].length-1);
-    let coordinateDirection = directionSplit[i].slice(directionSplit[i].length-1);
-    if ((coordinateDirection == "S") || (coordinateDirection == "W")){
-      heading = -1;
-    }else{
-      heading = 1;
-    }
-    if (directionSplit[i].includes("N") || directionSplit[i].includes("S")){
-      northSouth += coordinateNumber*heading;
-    } else {
-      eastWest += coordinateNumber*heading;
-    }
-  }
-  console.log("["+eastWest+","+northSouth+"]");
-  //return [eastWest,northSouth];
-  //return [0,1]
-}
-movements("3N 12S");
-=======
 !function(){
 
   const findCoordinates = (str) => {
@@ -50,4 +24,3 @@ movements("3N 12S");
   console.log(testCoor);
 
 }();
->>>>>>> bc36b6de7c1482a3e8e255c97dd179697e2a9b54

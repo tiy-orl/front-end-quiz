48a93ccb73ea8bba8cff063d64ef1c078e7d5acb
ddsheard <ddsheard@gmail.com>
2017-04-10 10:12:46 -0400

    update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,14 @@
+let printValue;
+for (i=1; i<=100; i++){
+  if (i%15 == 0){
+    printValue = "fizzbuzz";
+  }else if (i%3 == 0){
+    printValue = "fizz";
+  }else if (i%5 == 0){
+    printValue = "buzz";
+  }else {
+    printValue = i;
+  }
+  console.log(printValue);
 
-for (var i = 1; i <= 100; i++) {
-  if (i%3 === 0 && i%5 === 0) {
-    console.log("fizzbuzz");
-  }
-  else if (i%3 === 0 ) {
-    console.log("fizz");
-  }
-  else if (i%5 === 0) {
-    console.log("buzz");
-  }
-  else {
-    console.log(i);
-  }
 }

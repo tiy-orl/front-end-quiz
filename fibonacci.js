e4cd1ef6526a957a0ee4a72ff933054c0548b185
Tyler Davis <tyjdavis@outlook.com>
2017-04-12 09:31:33 -0400

    changes

--- fibonacci.js
+++ fibonacci.js
@@ -5,7 +5,7 @@ fib[1] = 1;
 for(i=2; i<=33; i++)
 {
 
-  fib[i] = fib[i-2] + fib[i-1];
+fib[i] = fib[i-2] + fib[i-1];
     //console.log(fib[i]);
 }
 
@@ -15,6 +15,7 @@ var evens = fib.filter( number => number % 2 === 0);
 var sum = 0;
 for (var i = 0; i < evens.length; i++){
   sum += evens[i]
+  
 }
 
 

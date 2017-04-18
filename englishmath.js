a9ce05353d5e14c4073c01716997ef2e0570ec37
Dwane <Dwane@hiphopandcode.com>
2017-04-18 10:16:28 -0400

    issue #86 - cleaned up more code

--- englishmath.js
+++ englishmath.js
@@ -2,4 +2,4 @@ function englishMath(equation){
   let equationSplit= equation.split(' plus ');
   return englishify(numberify(equationSplit[0]) + numberify(equationSplit[1]));
 }
-console.log(englishMath('ninety nine plus one hundred'));
+console.log(englishMath('ninety nine plus zero'));

5969bccd8b3bd65d6d5899f33752b867eb1b6df7
Chris Lebbano <chris.lebbano@gmail.com>
2017-04-19 09:23:18 -0400

    Wed quiz;

--- englishMath.js
+++ englishMath.js
@@ -7,13 +7,37 @@
 * englishMath('five plus seventeen') //=> 'twenty two'
 **********************************/
 
-const numberify = require('./numberify.js');
-const englishify = require('./englishify.js')
 
 function englishMath(str){
-  let arr =  str.split('plus');
-  let result = parseInt(numberify(arr[0].trim())) + parseInt(numberify(arr[1].trim()));
+  let result;
+  if (str.includes('plus')){
+    let arr =  str.split('plus');
+    result = parseInt(numberify(arr[0].trim())) + parseInt(numberify(arr[1].trim()));
+  }
+  if (str.includes('minus')){
+    let arr =  str.split('minus');
+    result = parseInt(numberify(arr[0].trim())) - parseInt(numberify(arr[1].trim()));
+  }
+  if (str.includes('times')){
+    let arr =  str.split('times');
+    result = parseInt(numberify(arr[0].trim())) * parseInt(numberify(arr[1].trim()));
+  }
+  if (str.includes('divide')){
+    let arr =  str.split('divide');
+    result = parseInt(numberify(arr[0].trim())) / parseInt(numberify(arr[1].trim()));
+  }
   return englishify(result);
 }
 
-console.log(englishMath('one hundred plus fifty five'));
+let $input = $('#math-input');
+let $button = $('label button');
+
+$button.on('click', function(event){
+  let val = $input.val();
+  let result = englishMath(val);
+  $input.val(result);
+  $.get(`http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=${result}`, function(data){
+    let $img = $('#giphy');
+    $img.attr('src', data.data.image_url);
+  });
+});
